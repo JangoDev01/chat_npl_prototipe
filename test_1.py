bd39f85998718174c5b8f0070e7bca89536e03bb
JangoDev01 <miguelgabriel072001@gmail.com>
2026-03-26 20:04:17 +0100

    criando uma interacao sobre os tokens do documentos

--- test_1.py
+++ test_1.py
@@ -10,10 +10,26 @@
 """
 
 import spacy
+import pt_core_news_sm as pt_data
 
 
+## variavel que armazena o modelo de linguagem em português
 pln = spacy.load("pt_core_news_sm")
-import pt_core_news_sm
-pln = pt_core_news_sm.load()
-doc = pln("Esta é uma frase de teste simples.")
-print([(w.text, w.pos_) for w in doc])
+pln = pt_data.load()
+
+user_input = input("Digite uma frase para análise: ")
+# u - prefixo para indicar que a string é uma string Unicode (importante para lidar com caracteres acentuados em português)
+doc = pln(f"{user_input}")
+
+
+"""
+    iteração sobre os tokens do documento e impressão de suas características (texto, parte do discurso, lema e dependência sintática)
+    pos_ - parte do discurso dos tokens (ex: substantivo, verbo, adjetivo, etc)
+    lemma_ - forma base ou canônica do token (ex: "correr" para "correndo")... Útil para análise de texto, pois permite agrupar palavras com a mesma raiz.
+    dep_ - relação de dependência sintática do token em relação a outros tokens na frase (ex: sujeito, objeto, predicado, etc).
+"""
+for token in doc:
+    print(f"Token: {token.text}, POS: {token.pos_}, Lemma: {token.lemma_}, Dep: {token.dep_}")
+
+## imprime uma lista de tuplas contendo o texto do token e sua respectiva parte do discurso (POS)
+# print([(w.text, w.pos_) for w in doc])
