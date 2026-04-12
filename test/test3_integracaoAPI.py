6ea58b5a292200b12420dd46e14c0f43ee377f31
JangoDev01 <miguelgabriel072001@gmail.com>
2026-04-12 10:24:53 +0100

    criando o arquivo test3 integracaoAPI - arquivo pra testar integracao de APIs com o telegram. e instalando pip3 install pyTelegramBotAPI, pip3 install telebot e pip3 install python-decouple

---
+++ test/test3_integracaoAPI.py
@@ -0,0 +1,8 @@
+"""
+    pip3 install pyTelegramBotAPI
+        - Biblioteca para criar bots no Telegram.
+    pip3 install telebot
+        - Biblioteca alternativa para criar bots no Telegram.
+    pip3 install python-decouple
+        - Biblioteca para gerenciar variáveis de ambiente de forma segura.
+"""
