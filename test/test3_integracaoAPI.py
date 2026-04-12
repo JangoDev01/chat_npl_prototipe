6d1d11a1509abc98569e4f4d9524c72677568c90
JangoDev01 <miguelgabriel072001@gmail.com>
2026-04-12 18:40:33 +0100

    criando o arquivo .env - pra guardar a chave do bot e dar um grau acima de seguranca. comecando a primeir interacao com o bot ja integrado com o telegram

--- test/test3_integracaoAPI.py
+++ test/test3_integracaoAPI.py
@@ -6,3 +6,31 @@
     pip3 install python-decouple
         - Biblioteca para gerenciar variáveis de ambiente de forma segura.
 """
+
+import telebot 
+
+"""
+    from decouple import config:
+        - Importa a função 'config' da biblioteca 'decouple' para acessar variáveis de ambiente.
+        - Permite que o token do bot seja armazenado de forma segura em um arquivo .env, evitando expor informações sensíveis no código.
+
+"""
+from decouple import config
+
+"""
+    
+"""
+token = config('TOKENBOT_TESTE')
+bot = telebot.TeleBot(token)
+
+###### PRIMEIRA ETAPA - Start Bot ######
+
+# Define um manipulador de mensagens para o comando "/start". Quando um usuário enviar esse comando, a função 'start' será chamada...
+@bot.message_handler(commands=["start"])
+
+# A função 'start' é responsável por enviar uma mensagem de boas-vindas ao usuário...
+def start(message):
+    bot.send_message(message.chat.id, "Mekié nerd! Eu sou Beringelus o bot nerd. Como posso ajudar você hoje?", timeout=60)
+
+# Faz uma sondagem contínua para verificar se há novas mensagens...
+bot.polling()
